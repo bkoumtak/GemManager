17dc24d4867c0deec797a741c0b5efb534507d1a
bkoumtak <bkoumtak@gmail.com>
2020-01-29 00:33:35 -0500

    Test adding users to list

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 
-export class Counter extends Component {
-  static displayName = Counter.name;
+export class UserManagement extends Component {
+    static displayName = "User Management";
 
-  constructor(props) {
-    super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { user:'', users: [] };
+        this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this); 
+    }
 
-  incrementCounter() {
-    this.setState({
-      currentCount: this.state.currentCount + 1
-    });
-  }
+    handleChange(e) {
+        this.setState({
+            user: e.target.value
+        }); 
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Counter</h1>
+    addUser() {
+        let usersCopy = this.state.users.slice(); 
+        usersCopy.push(this.state.user); 
+        //console.log(usersCopy); 
+        this.setState({
+            users: usersCopy
+        });
+        
+    }
 
-        <p>This is a simple example of a React component.</p>
+    
+    render() {
+        let users = this.state.users.slice(); 
+        const listOfUsers = users.map((user, index) =>
+            <li key={index}>
+                {user}
+            </li>
+        );
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+        return (
+            <div>
+            <input type="text" onChange={this.handleChange}/>
+                <div className="pt-2">
+                <button className="btn btn-primary" onClick={this.addUser}>Add User</button>
+                </div>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
-      </div>
-    );
-  }
+                <div className="pt-2">
+                    <ul> {listOfUsers} </ul>
+                </div>
+            </div>
+        );
+    }
 }
