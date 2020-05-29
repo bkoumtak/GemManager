0dbcbf8a5d66ef8d77e004279ea079e323a8f75a
aponomarev <aponomarev@genetec.com>
2020-05-29 01:32:18 -0400

    fixed the blank page bug & made the of the db not accessbile more verbose

--- ClientApp/src/_helpers/handle-response.js
+++ ClientApp/src/_helpers/handle-response.js
@@ -1,8 +1,6 @@
-﻿import { authenticationService } from '../_services/authentication.service'; 
-
-export function handleResponse(response) {
+﻿export function handleResponse(response) {
     return response.text().then(text => {
-        const data = text && JSON.parse(text); 
+        const data = text && text.indexOf('gems.db') == -1 && JSON.parse(text); 
         if (!response.ok) {
             if ([401, 403].indexOf(response.status) !== -1) {
                 this.props.history.push('/');  
@@ -13,6 +11,10 @@ export function handleResponse(response) {
             if ([400].indexOf(response.status) !== -1) {
                 window.location.reload(true);
                 error = data;
+            }
+
+            if ([500].indexOf(response.status) !== -1) {
+                error = 'The database doesn\'t seem to be accesible. Please try again later';
             }
 
             return Promise.reject(error); 
